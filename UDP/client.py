03cd6769195540f41f1ac0be2efffd5496b4167c
slinglrz <35695532+slinglrz@users.noreply.github.com>
2018-01-23 04:29:24 +0700

    Update client.py

--- UDP/client.py
+++ UDP/client.py
@@ -2,15 +2,15 @@ import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5008
-MESSAGE = "3"
+MESSAGER = "3"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print "message:", MESSAGER
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(MESSAGER, (UDP_IP, UDP_PORT))
 
 data, server = sock.recvfrom(1024)
 print "Answer =",data
